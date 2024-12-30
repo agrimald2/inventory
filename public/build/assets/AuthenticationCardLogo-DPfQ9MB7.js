de054282eacd857375830c49b7d55acd929cd061
agrimald2 <agrimaldopci18@gmail.com>
2024-12-30 01:29:19 -0300

    uild

--- public/build/assets/AuthenticationCardLogo-DPfQ9MB7.js
+++
@@ -1,1 +0,0 @@
-import{o,c as e,w as t,a as s,u as r,j as n}from"./app-8SJXMQXs.js";const _={__name:"AuthenticationCardLogo",setup(c){return(l,a)=>(o(),e(r(n),{href:"/"},{default:t(()=>a[0]||(a[0]=[s("img",{src:"/logo.jpg",alt:"logo",class:"w-32 h-32"},null,-1)])),_:1}))}};export{_};
