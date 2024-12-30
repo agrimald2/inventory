dd84a35fee7d064421b85b0c72908b2355f0a1a3
agrimald2 <agrimaldopci18@gmail.com>
2024-12-30 01:32:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,7 @@ use App\Http\Controllers\InventoryController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 Route::middleware([
